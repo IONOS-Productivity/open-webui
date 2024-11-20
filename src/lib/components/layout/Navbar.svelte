30772289f5d3e193b54e6fd7e7feb1ccee970df9
Misha M.-Kupriyanov <kupriyanov@strato.de>
2024-11-20 14:51:19 +0100

    feat(ionos-header): disable model selector
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -34,7 +34,7 @@
 
 	export let chat;
 	export let selectedModels;
-	export let showModelSelector = true;
+	export let showModelSelector = false;
 
 	let showShareChatModal = false;
 	let showDownloadChatModal = false;
