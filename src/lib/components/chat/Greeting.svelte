e2015d901f82b367249a0815bb8a7c84ca0dffae
Thomas Lehmann <t.lehmann@strato.de>
2024-11-21 13:56:45 +0100

    refactor: extract greeting as own component
    * Slimline the Placeholder component
    * Allow customization in "own space"

---
+++ src/lib/components/chat/Greeting.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { getContext } from 'svelte';
+import { fade } from 'svelte/transition';
+
+import { user } from '$lib/stores';
+
+const i18n = getContext('i18n');
+
+export let greeting: string | null = null;
+</script>
+
+<div class=" text-3xl sm:text-4xl line-clamp-1" in:fade={{ duration: 100 }}>
+	{#if greeting}
+		{greeting}
+	{:else}
+		{$i18n.t('Hello, {{name}}', { name: $user.name })}
+	{/if}
+</div>
