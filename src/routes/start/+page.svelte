540a4eb4b83aa8ccca3c1d952aed460e68636615
Thomas Lehmann <t.lehmann@strato.de>
2024-11-21 11:48:18 +0100

    customize: start page as custom page/route
    == Setting / scope
    
    This page is meant as entrypoint for the public IONOS GPT demo page
    before the user has a session.
    
    == Navigation
    
    goto() was deliberrately avoided and window.location to bypass the
    router and just start with a fresh page.
    
    == Expectation
    
    The page does not expect not try to start an Open WebUI session.
    
    == Limitation
    
    Default model still hardcoded. According to ##151 this needs to be
    provided by a config.

---
+++ src/routes/start/+page.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+
+/**
+ * Landing page for IONOS GPT public demo.
+ *
+ * This page should be served as a resource from /start.
+ *
+ * The user should be redirected to this page if they do not have an
+ * internal session yet.
+ *
+ * The user would then load a new resource as part of the Open WebUI SPA so
+ * that a transparent sign-in mechanism can run.
+ *
+ * The router should avoid loading Open WebUI API resources like
+ * authentication and Websockets.
+ */
+
+import { getContext } from 'svelte';
+import { config } from '$lib/stores';
+
+import Greeting from '$lib/components/chat/Greeting.svelte';
+import Suggestions from '$lib/components/chat/Suggestions.svelte';
+import PaperPlane from '$lib/components/icons/PaperPlane.svelte';
+import Textarea from '$lib/components/common/Textarea.svelte';
+import Trailer from '$lib/IONOS/components/Trailer.svelte';
+
+const i18n = getContext('i18n');
+
+// TODO hardcoded during early development, has to be provided by a
+// suggested prompts + model config
+const model = 'meta-llama/Meta-Llama-3.1-8B-Instruct';
+
+let prompt = '';
+
+function submit(promptToStart: string, modelNameForStart: string) {
+	// Pass prompt and model securely via sessionStorage and start chat by
+	// *hard* page load, deliberately circumventing the push state API
+	sessionStorage.selectedModels = JSON.stringify([modelNameForStart]);
+	sessionStorage.ionosGptDemoPrompt = promptToStart;
+
+	window.location = `/`;
+}
+</script>
+
+<div class="overflow-auto w-full h-full flex flex-col items-center">
+	<Greeting />
+
+	<form
+		on:submit|preventDefault={(e) => submit(prompt, model)}
+		class="flex items-center min-w-96 h-fit my-4 border rounded-xl"
+		>
+
+		<Textarea
+			bind:value={prompt}
+			placeholder={$i18n.t('Send a message to IONOS GPT', { ns: "ionos" })}
+			className="w-full rounded-lg p-3 text-sm dark:text-gray-300 dark:bg-gray-850 outline-none resize-none h-full bg-transparent"
+		/>
+
+		<button type="submit" class="p-0 mx-2 flex w-8">
+			<PaperPlane className="fill-gray-400 cursor-pointer hover:fill-ionos" />
+		</button>
+	</form>
+
+	<div class="mx-5">
+		<Suggestions
+			suggestionPrompts={$config?.default_prompt_suggestions ?? []}
+			on:select={(e) => submit(e.detail, model)}
+		/>
+	</div>
+
+	<Trailer />
+</div>
