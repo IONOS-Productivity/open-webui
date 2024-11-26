9b9a5068876e6fab04516677e17ac2ed71b752fa
Thomas Lehmann <t.lehmann@strato.de>
2024-11-26 15:17:20 +0100

    fix(Greeting): center welcome headline

--- src/lib/components/chat/Greeting.svelte
+++ src/lib/components/chat/Greeting.svelte
@@ -8,6 +8,6 @@ export let greeting: string | null = null;
 </script>
 
 <div in:fade={{ duration: 100 }}>
-	<h1 class="text-2xl sm:text-3xl font-bold line-clamp-1 text-ionos">{$i18n.t('Welcome at IONOS GPT', { ns: 'ionos' })}</h1>
+	<h1 class="text-2xl text-center  sm:text-3xl font-bold line-clamp-1 text-ionos">{$i18n.t('Welcome at IONOS GPT', { ns: 'ionos' })}</h1>
 	<h4 class="text-base line-clamp-1">{$i18n.t('Your first step into the world of AI! Privacy by Design!', { ns: 'ionos' })}</h4>
 </div>
