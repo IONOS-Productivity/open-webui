c0cefe906ba3957605a313506c55796db2ad1c60
Thomas Lehmann <t.lehmann@strato.de>
2024-11-27 18:03:36 +0100

    customize(Suggestions): provide prompt suggestions
    Customize prompt suggestion page with prompt and model tuple.
    
    The suggestions shall be displayed with a short title and description
    and an icon of editor choice. We opted for configuring an icon URL for
    now, which can also be a data: URL.
    
    Language can be DE-only at first, so i18n was ignored for now.
    
    Refs: ##151

---
+++ src/lib/IONOS/components/Suggestions.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { getContext, createEventDispatcher } from 'svelte';
+	import prompts from '$lib/IONOS/configs/ionosPromptSuggestions.json'
+
+	const i18n = getContext('i18n');
+	const dispatch = createEventDispatcher();
+
+	export let className = '';
+</script>
+
+<div class="flex flex-wrap justify-center {className}">
+	{#each prompts as prompt, promptIdx}
+		<button
+			class="flex flex-col shrink-0 basis-64 w-52 justify-between items-center px-3 py-2 rounded-xl bg-transparent hover:bg-black/5 hover:text-ionos dark:hover:bg-white/5 transition group"
+			on:click={() => {
+				dispatch('select', { prompt: prompt.content, model: prompt.model });
+			}}
+		>
+		<div class="text-xl h-10">{prompt.description}</div>
+		<img class="w-32 h-14 m-4" src={prompt.iconUrl} />
+		<div class="h-10">{prompt.detail}</div>
+		</button>
+	{/each}
+</div>
