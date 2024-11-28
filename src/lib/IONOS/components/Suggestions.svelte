5591dd900c327d65e756eb53481335b3b4e642cb
Thomas Lehmann <t.lehmann@strato.de>
2024-11-28 10:03:10 +0100

    customize(Suggestions): change style according to PM feedback

--- src/lib/IONOS/components/Suggestions.svelte
+++ src/lib/IONOS/components/Suggestions.svelte
@@ -10,14 +10,14 @@
 <div class="flex flex-wrap justify-center {className}">
 	{#each prompts as prompt, promptIdx}
 		<button
-			class="flex flex-col shrink-0 basis-64 w-52 justify-between items-center px-3 py-2 rounded-xl bg-transparent hover:bg-black/5 hover:text-ionos dark:hover:bg-white/5 transition group"
+			class="flex flex-col shrink-0 basis-64 w-52 justify-between items-center px-3 py-2 rounded-xl bg-transparent border m-1 hover:bg-black/5 hover:text-ionos dark:hover:bg-white/5 transition group"
 			on:click={() => {
 				dispatch('select', { prompt: prompt.content, model: prompt.model });
 			}}
 		>
-		<div class="text-xl h-10">{prompt.description}</div>
-		<img class="w-32 h-14 m-4" src={prompt.iconUrl} />
-		<div class="h-10">{prompt.detail}</div>
+			<div class="flex-1 basis-10 text-base">{prompt.description}</div>
+			<div class="flex-1 basis-10 text-sm">{prompt.detail}</div>
+			<img class="w-32 h-10 m-4" src={prompt.iconUrl} />
 		</button>
 	{/each}
 </div>
