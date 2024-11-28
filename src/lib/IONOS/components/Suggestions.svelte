291bf21302e4dc1d18ae96c31f80fa9b481d4c32
Thomas Lehmann <t.lehmann@strato.de>
2024-11-28 10:17:56 +0100

    [POC] customize(Suggestions): icon hover effect using mask-image
    This would only support SVG.

--- src/lib/IONOS/components/Suggestions.svelte
+++ src/lib/IONOS/components/Suggestions.svelte
@@ -9,15 +9,36 @@
 
 <div class="flex flex-wrap justify-center {className}">
 	{#each prompts as prompt, promptIdx}
-		<button
-			class="flex flex-col shrink-0 basis-64 w-52 justify-between items-center px-3 py-2 rounded-xl bg-transparent border m-1 hover:bg-black/5 hover:text-ionos dark:hover:bg-white/5 transition group"
+	<div class="m-1 flex flex-col flex-wrap flex-1 shrink-0 basis-52 h-50">
+		<input
+			id={"model-" + promptIdx}
+			name="model"
+			type="radio"
 			on:click={() => {
 				dispatch('select', { prompt: prompt.content, model: prompt.model });
 			}}
-		>
-			<div class="flex-1 basis-10 text-base">{prompt.description}</div>
-			<div class="flex-1 basis-10 text-sm">{prompt.detail}</div>
-			<img class="w-32 h-10 m-4" src={prompt.iconUrl} />
-		</button>
+		/>
+		<label for={"model-" + promptIdx} class="flex flex-col items-center h-full content px-3 py-2 rounded-xl bg-transparent border-2 border-gray-200 hover:bg-black/5 hover:text-ionos dark:hover:bg-white/5 transition">
+			<div class="flex-1 text-center text-base">{prompt.description}</div>
+			<div class="flex-1 text-center text-sm mt-2">{prompt.detail}</div>
+			<div class="image shrink-1 w-10 h-10 m-4 masked bg-gray-700" style={`mask-image: url(${prompt.iconUrl})`} />
+		</label>
+	</div>
 	{/each}
 </div>
+
+<style>
+input {
+	display: none;
+}
+.masked {
+	mask-repeat: no-repeat;
+	mask-position: center;
+}
+input:hover .image {
+	background-color: var(--ion-brand-color);
+}
+input:checked + label {
+	border: 2px solid var(--ion-brand-color);
+}
+</style>
