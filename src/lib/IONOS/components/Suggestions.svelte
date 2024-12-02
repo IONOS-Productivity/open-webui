468b5380b7533a9579171edfa88f56e5ef8d3a15
Thomas Lehmann <t.lehmann@strato.de>
2024-12-02 14:46:44 +0100

    customize(Suggestions): change suggestion options from radio to button

--- src/lib/IONOS/components/Suggestions.svelte
+++ src/lib/IONOS/components/Suggestions.svelte
@@ -15,18 +15,16 @@
 <div class="flex flex-wrap md:flex-row md:flex-wrap justify-center {className}">
 	{#each promptSelection as prompt, promptIdx}
 	<div class="m-1 flex flex-col md:flex-wrap shrink-0 md:basis-52 h-50 w-full md:w-auto">
-		<input
-			id={"model-" + promptIdx}
-			name="model"
+		<button
 			type="radio"
+			class="flex flex-col items-center h-full content px-3 py-2 rounded-xl bg-transparent border-2 border-gray-200 hover:bg-black/5 hover:text-ionos dark:hover:bg-white/5 transition"
 			on:click={() => {
 				dispatch('select', { prompt: prompt.content, model: prompt.model, modelName: prompt.modelName });
 			}}
-		/>
-		<label for={"model-" + promptIdx} class="flex flex-col items-center h-full content px-3 py-2 rounded-xl bg-transparent border-2 border-gray-200 hover:bg-black/5 hover:text-ionos dark:hover:bg-white/5 transition">
+		>
 			<div class="flex-1 text-center text-base">{prompt.description}</div>
 			<div class="flex-1 text-center text-xs mt-2">{prompt.detail}</div>
-		</label>
+		</button>
 	</div>
 	{/each}
 </div>
