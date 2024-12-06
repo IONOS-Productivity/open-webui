92061c8763b2c22e605f8c96c63f2fe4417c3c5e
Thomas Lehmann <t.lehmann@strato.de>
2024-12-06 10:24:08 +0100

    customize(router): make i18n init independent from backend config
    We don't need to configure the default language for this demo
    application depending on a backend config, browser detection
    should suffice.
    
    There were paths that lead to an error due to backendConfig being
    undefined in this scope. This was an oversight of mine introduced
    with
    
       f5fb430583d5b54720ce55c7d339d1f9b8523e17
       chore(router): move setting language to function
    
       Prepare reusing the function later.
    
    == Steps to reproduce
    
    1. Clear localStorage or remove "locale" entry
    2. Open http://localhost:3000/
    3. Observe error "backendConfig is not defined"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -49,9 +49,7 @@
 		const browserLanguages = navigator.languages
 			? navigator.languages
 			: [navigator.language || navigator.userLanguage];
-		const lang = backendConfig.default_locale
-			? backendConfig.default_locale
-			: bestMatchingLanguage(languages, browserLanguages, 'en-US');
+		const lang = bestMatchingLanguage(languages, browserLanguages, 'en-US');
 		$i18n.changeLanguage(lang);
 	};
 

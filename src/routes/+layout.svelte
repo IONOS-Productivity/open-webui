IONOS-Productivity/open-webui

commit f5fb430583d5b54720ce55c7d339d1f9b8523e17

chore(router): move setting language to function
Prepare reusing the function later.

src/routes/+layout.svelte
@@ -41,6 +41,21 @@
 	let loaded = false;
 	const BREAKPOINT = 768;
 
+	const setLanguage = async () => {
+		if (localStorage.locale) {
+			return;
+		}
+
+		const languages = await getLanguages();
+		const browserLanguages = navigator.languages
+			? navigator.languages
+			: [navigator.language || navigator.userLanguage];
+		const lang = backendConfig.default_locale
+			? backendConfig.default_locale
+			: bestMatchingLanguage(languages, browserLanguages, 'en-US');
+		$i18n.changeLanguage(lang);
+	};
+
 	const setupSocket = () => {
 		const _socket = io(`${WEBUI_BASE_URL}` || undefined, {
 			reconnection: true,
@@ -112,16 +127,7 @@
 		// so `/error` can show something that's not `undefined`.
 
 		initI18n();
-		if (!localStorage.locale) {
-			const languages = await getLanguages();
-			const browserLanguages = navigator.languages
-				? navigator.languages
-				: [navigator.language || navigator.userLanguage];
-			const lang = backendConfig.default_locale
-				? backendConfig.default_locale
-				: bestMatchingLanguage(languages, browserLanguages, 'en-US');
-			$i18n.changeLanguage(lang);
-		}
+		await setLanguage();
 
 		if (backendConfig) {
 			// Save Backend Status to Store

commit 92061c8763b2c22e605f8c96c63f2fe4417c3c5e

customize(router): make i18n init independent from backend config
We don't need to configure the default language for this demo
application depending on a backend config, browser detection
should suffice.

There were paths that lead to an error due to backendConfig being
undefined in this scope. This was an oversight of mine introduced
with

   f5fb430583d5b54720ce55c7d339d1f9b8523e17
   chore(router): move setting language to function

   Prepare reusing the function later.

== Steps to reproduce

1. Clear localStorage or remove "locale" entry
2. Open http://localhost:3000/
3. Observe error "backendConfig is not defined"

src/routes/+layout.svelte
@@ -49,9 +49,7 @@
 		const browserLanguages = navigator.languages
 			? navigator.languages
 			: [navigator.language || navigator.userLanguage];
-		const lang = backendConfig.default_locale
-			? backendConfig.default_locale
-			: bestMatchingLanguage(languages, browserLanguages, 'en-US');
+		const lang = bestMatchingLanguage(languages, browserLanguages, 'en-US');
 		$i18n.changeLanguage(lang);
 	};
 