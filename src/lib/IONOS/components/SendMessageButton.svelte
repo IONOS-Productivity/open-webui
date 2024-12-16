2ac17033c11b012f0e8911eb94a099adde243ab2
Misha M.-Kupriyanov <kupriyanov@strato.de>
2024-12-16 12:58:09 +0100

    customize(SendMessageButton): extract as own component
    in order to restyle later
    
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

---
+++ src/lib/IONOS/components/SendMessageButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import PaperPlane from '$lib/components/icons/PaperPlane.svelte';
+
+	export let disabled = false;
+</script>
+
+<button {disabled} type="submit" class="{$$restProps.class || ''}">
+	<PaperPlane
+		className="fill-gray-400 group-disabled/button:fill-gray-400 cursor-pointer group-disabled/button:cursor-default hover:fill-ionos"
+	/>
+</button>
+
+<style>
+	button {
+		transition: background-color 0.2s, color 0.2s;
+	}
+
+	button:disabled {
+		pointer-events: none;
+	}
+</style>
