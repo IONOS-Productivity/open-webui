50ce8ea8867cebfe102aed763e8b501ab880837b
Misha M.-Kupriyanov <kupriyanov@strato.de>
2024-12-16 14:19:26 +0100

    customize(Suggestions): change according UX
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- src/lib/IONOS/components/Suggestions.svelte
+++ src/lib/IONOS/components/Suggestions.svelte
@@ -6,6 +6,7 @@
 	const dispatch = createEventDispatcher();
 
 	const maxSuggestionsToDisplay = 3;
+	let hoveredId = 0;
 
 	// Filter for model
 	export let model;
@@ -17,18 +18,24 @@
 
 <div class="flex flex-wrap md:flex-row md:flex-wrap justify-center {className}">
 	{#each promptSelection as prompt, promptIdx}
-	<div class="m-1 flex flex-col md:flex-wrap shrink-0 md:basis-52 h-50 w-full md:w-auto">
-		<button
-			type="radio"
-			class="flex flex-col items-center h-full content px-3 py-2 rounded-xl bg-transparent border-2 border-gray-200 hover:bg-black/5 hover:text-ionos dark:hover:bg-white/5 transition"
-			on:click={() => {
-				dispatch('select', { prompt: prompt.content, model: prompt.model, modelName: prompt.modelName });
-			}}
+		<div class="exos-card exos-__direct-selection m-1 flex flex-col md:flex-wrap shrink-0 md:basis-52 h-50 w-full md:w-auto"
+			class:exos-__direct-selection--hover={hoveredId === promptIdx}
+				 on:mouseenter={() => (hoveredId = promptIdx)}
+				 on:mouseleave={() => (hoveredId = null)}
 		>
-			<div class="flex-1 text-center text-base">{prompt.description}</div>
-			<div class="flex-1 text-center text-xs mt-2">{prompt.detail}</div>
-		</button>
-	</div>
+			<button
+					type="radio"
+					class="exos-card__content flex h-full content px-3 py-2"
+					on:click={() => {
+						dispatch('select', { prompt: prompt.content, model: prompt.model, modelName: prompt.modelName });
+					}}
+			>
+				<section class="exos-card__section">
+					<h2 class="exos-card__headline">{prompt.description}</h2>
+					<p class="exos-paragraph">{prompt.detail}</p>
+				</section>
+			</button>
+		</div>
 	{/each}
 </div>
 
@@ -45,5 +52,9 @@ input:hover .image {
 }
 input:checked + label {
 	border: 2px solid var(--ion-brand-color);
+}
+.exos-card .exos-card__section .exos-card__headline{
+	color: var(--interactive-text-color);
+	font-size: 14px;
 }
 </style>
