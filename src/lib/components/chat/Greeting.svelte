4b48a146fae15d833c3e3455d51b7dcb342a1771
Misha M.-Kupriyanov <kupriyanov@strato.de>
2024-12-16 13:35:42 +0100

    chore(prettier): reformat Greeting component
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- src/lib/components/chat/Greeting.svelte
+++ src/lib/components/chat/Greeting.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-import { getContext } from 'svelte';
-import { fade } from 'svelte/transition';
+	import { getContext } from 'svelte';
+	import { fade } from 'svelte/transition';
 
-const i18n = getContext('i18n');
+	const i18n = getContext('i18n');
 
-export let greeting: string | null = null;
+	export let greeting: string | null = null;
 </script>
 
 <div in:fade={{ duration: 100 }}>
-	<h1 class="text-2xl text-center  sm:text-3xl font-bold line-clamp-1 text-ionos">{$i18n.t('Welcome at IONOS GPT', { ns: 'ionos' })}</h1>
-	<h4 class="text-base line-clamp-1">{$i18n.t('Your first step into the world of AI! Privacy by Design!', { ns: 'ionos' })}</h4>
+	<h1 class="text-2xl text-center sm:text-3xl font-bold line-clamp-1 text-ionos">
+		{$i18n.t('Welcome at IONOS GPT', { ns: 'ionos' })}
+	</h1>
+	<h4 class="text-base line-clamp-1">
+		{$i18n.t('Your first step into the world of AI! Privacy by Design!', { ns: 'ionos' })}
+	</h4>
 </div>
