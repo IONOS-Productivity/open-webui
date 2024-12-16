8976dbe78397fda4ca2dfa009c9167058bc4c385
Kai Henseler <kai.henseler@strato.de>
2024-12-16 17:08:55 +0100

    fix(ModelSelector): click on tile does not change model
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- src/lib/IONOS/components/ModelSelector.svelte
+++ src/lib/IONOS/components/ModelSelector.svelte
@@ -20,28 +20,26 @@
 
 <form class="flex flex-row flex-wrap justify-center {$$restProps.class}">
 	{#each models as currentModel, promptIdx}
-		<button class="flex flex-1 exos-tile mx-3 !mb-0" type="button">
-			<input
-				bind:group={model}
-				id={'model-' + promptIdx}
-				value={currentModel[0]}
-				name="model"
-				type="radio"
-			/>
-			<label for={'model-' + promptIdx}>
-				<ModelIcons class="exos-tile__image" selectedModel={currentModel[0]} />
-				<span class="exos-tile__description break-normal"> {currentModel[1]}</span>
-			</label>
-		</button>
+		<input
+			bind:group={model}
+			id={'model-' + promptIdx}
+			value={currentModel[0]}
+			name="model"
+			type="radio"
+			class="hidden"
+		/>
+		<label
+			for={'model-' + promptIdx}
+			class="flex flex-1 exos-tile mx-3 !mb-0 {model === currentModel[0]
+				? '!bg-[var(--hovered-tile-background-color)]'
+				: ''}"
+		>
+			<ModelIcons class="exos-tile__image" selectedModel={currentModel[0]} />
+			<span class="exos-tile__description break-normal"> {currentModel[1]}</span>
+		</label>
 	{/each}
 </form>
 
 <div class="mt-1">
 	<p class="exos-headline exos-headline--paragraph exos-headline--activating">{modelName}</p>
 </div>
-
-<style>
-	input {
-		display: none;
-	}
-</style>
