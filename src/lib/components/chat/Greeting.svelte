a80598b7e6f09daa9f4c9fbd0a187dec8f8e70aa
Misha M.-Kupriyanov <kupriyanov@strato.de>
2024-12-16 13:56:40 +0100

    customize(Greeting): change according UX
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- src/lib/components/chat/Greeting.svelte
+++ src/lib/components/chat/Greeting.svelte
@@ -8,10 +8,7 @@
 </script>
 
 <div in:fade={{ duration: 100 }}>
-	<h1 class="text-2xl text-center sm:text-3xl font-bold line-clamp-1 text-ionos">
+	<h1 class="text-center exos-page-header sm:text-3xl line-clamp-1">
 		{$i18n.t('Welcome at IONOS GPT', { ns: 'ionos' })}
 	</h1>
-	<h4 class="text-base line-clamp-1">
-		{$i18n.t('Your first step into the world of AI! Privacy by Design!', { ns: 'ionos' })}
-	</h4>
 </div>
