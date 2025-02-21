bfe26d8f225925ef55cbfef7947144676eb535a7
Thomas Lehmann <t.lehmann@strato.de>
2025-02-21 09:23:00 +0100

    [WIP] Add Scroller component skeleton
    Development paused due to accessbility concerns.

---
+++ src/lib/IONOS/components/Scroller.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { createEventDispatcher, onMount } from 'svelte';
+	import type { ScrollerItem } from './scrollerItem.d.ts';
+
+	const dispatch = createEventDispatcher();
+
+	export const tickSpeed: number = 100;
+	export let items: ScrollerItem[] = [];
+</script>
+
+<div class="overflow-hidden">
+	<div class="m-4">
+		{#each items as { id, text }}
+			<button
+				class="fw-60 py-4 px-2 bg-white max-w-64 text-sm"
+				data-id={id}
+				on:click={() => {
+					dispatch('click', id);
+				}}
+			>
+				{text} →
+			</button>
+		{/each}
+	</div>
+</div>
